51009e07fcf6effbf4d362fc2b00002dfb99ae93
Colby Horan <colbyhoran@gmail.com>
2020-08-22 09:57:51 -0500

    ready for deployment

--- src/app.jsx
+++ src/app.jsx
@@ -1,51 +1,52 @@
-import React, { Component } from 'react'
-import { FormGroup, FormControl, InputGroup } from 'react-bootstrap'
-import Photos from './components/photos'
-import AlertMessage from './components/alerts'
-import banner from "./thats-not-mars.png"
+import React, { Component } from "react";
+import { FormGroup, FormControl, InputGroup } from "react-bootstrap";
+import Photos from "./components/photos";
+import AlertMessage from "./components/alerts";
+import banner from "./thats-not-mars.png";
 
-import './style.css'
+import "./style.css";
 
-import moment from 'moment'
+import moment from "moment";
 
 // Used for reseting app
 const initialState = {
-  query: '',
+  query: "",
   earth_date: null,
-  alert: '',
+  alert: "",
   photos: [],
   isLoaded: false,
   items: []
-}
+};
 
 class App extends Component {
   constructor(props) {
-    super(props)
-    this.state = initialState
-    this.startOver = this.startOver.bind(this)
+    super(props);
+    this.state = initialState;
+    this.startOver = this.startOver.bind(this);
   }
 
   // API Call
   search() {
-    const base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
-    const key = 'AKjgN2ndVp2kkWjsY1JA6t2i5zE8FbyAYW6dqGks'
-    const fetch_url = `${base_url}earth_date=${this.state.query}&api_key=${key}`
+    const base_url =
+      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
+    const key = "AKjgN2ndVp2kkWjsY1JA6t2i5zE8FbyAYW6dqGks";
+    const fetch_url = `${base_url}earth_date=${this.state.query}&api_key=${key}`;
     if (!this.state.query) {
-      this.setState({alert:'error'})
+      this.setState({ alert: "error" });
     } else {
-      fetch(fetch_url, {method: "GET"})
+      fetch(fetch_url, { method: "GET" })
         .then(res => res.json())
         .then(json => {
-          const photos = json.photos
-          this.setState({photos, alert: 'success'})
-      })
+          const photos = json.photos;
+          this.setState({ photos, alert: "success" });
+        });
     }
   }
 
-// Resets the query and photo array
+  // Resets the query and photo array
   startOver() {
-    this.setState(initialState)
-    console.log(this.state)
+    this.setState(initialState);
+    console.log(this.state);
   }
 
   render() {
@@ -53,38 +54,47 @@ class App extends Component {
       <div className="app">
         <div className="app-title">
           {/* Yes I am aware that is a picture of Jupiter */}
-          <img className="app-title mb-5 logo" src={ banner } alt="null" />
+          <img className="app-title mb-5 logo" src={banner} alt="null" />
         </div>
         <div className="app-body container">
-          <FormGroup className='row justify-content-center d-block mx-0'>
-            <InputGroup className='input'>
+          <FormGroup className="row justify-content-center d-block mx-0">
+            <InputGroup className="input">
               <FormControl
-                type='date'
+                type="date"
                 value={this.state.query}
                 onChange={event => {
-                  this.setState({query: event.target.value})
+                  this.setState({ query: event.target.value });
                 }}
                 onKeyPress={event => {
-                  if (event.key === 'Enter') {
-                    this.search()
+                  if (event.key === "Enter") {
+                    this.search();
                   }
                 }}
                 max={moment().format("YYYY-MM-DD")}
               />
-              <span className='input-group-text bg-success' onClick={() => this.search()}>
+              <span
+                className="input-group-text bg-success"
+                onClick={() => this.search()}
+              >
                 <i className="fas fa-search"></i>
               </span>
-              <span className='input-group-text bg-danger' onClick={() => this.startOver()}>
+              <span
+                className="input-group-text bg-danger"
+                onClick={() => this.startOver()}
+              >
                 <i className="fas fa-redo-alt"></i>
               </span>
             </InputGroup>
           </FormGroup>
-          <AlertMessage alert={this.state.alert}/>
-          <Photos photos={ this.state.photos } className='justify-content-center'></Photos>
+          <AlertMessage alert={this.state.alert} />
+          <Photos
+            photos={this.state.photos}
+            className="justify-content-center"
+          ></Photos>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/alerts.jsx
+++ src/components/alerts.jsx
@@ -1,52 +1,61 @@
-import React, { Component, Fragment } from 'react'
-import Alert from 'react-bootstrap/Alert'
+import React, { Component, Fragment } from "react";
+import Alert from "react-bootstrap/Alert";
 
-import '../style.css'
+import "../style.css";
 
 // I built it like this to allow for multiple Alerts.  If this were a registration form, for example, it would allow placing messages in different places.
 
 class AlertMessage extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      show: true,
-    }
+      show: true
+    };
   }
   renderAlert() {
-    if(this.props.alert === 'error' && this.state.show){
+    if (this.props.alert === "error" && this.state.show) {
       // Displays Error Alert
       return (
-        <Alert variant="danger" className='row'>
+        <Alert variant="danger" className="row">
           <h3>Oh snap! You got an error!</h3>
           <p>
-            Change this and that and try again. Maybe reload the page? Maybe throw your computer in the pool? Is that Jupiter? Isn't this supposed to be the ~Mars~ Rover? YES, but that's not what's causing the error.
+            Change this and that and try again. Maybe reload the page? Maybe
+            throw your computer in the pool? Is that Jupiter? Isn't this
+            supposed to be the ~Mars~ Rover? YES, but that's not what's causing
+            the error.
           </p>
         </Alert>
-      )
-    } else if (this.props.alert === 'success' && this.state.show){
+      );
+    } else if (this.props.alert === "success" && this.state.show) {
       // Displays Success Alert
-      const handleDismiss = () => this.setState({ show: false })
-      return(
+      const handleDismiss = () => this.setState({ show: false });
+      return (
         <Alert dismissible variant="success" onClose={handleDismiss}>
-          <h3>Awww Yiss! You've successfully looked up pictures from the Mars Rover</h3>
+          <h3>
+            Awww Yiss! You've successfully looked up pictures from the Mars
+            Rover
+          </h3>
           <p>
-            Space: the final frontier. These are the photos of the Curiosity Mars Rover. Its continuing mission: to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before*.
+            Space: the final frontier. These are the photos of the Curiosity
+            Mars Rover. Its continuing mission: to explore strange new worlds,
+            to seek out new life and new civilizations, to boldly go where no
+            one has gone before*.
           </p>
-          <hr/>
-          <footer className='text-left text-muted'><small>*So far it has just explored Mars.  No future plans to explore other planets.</small></footer>
+          <hr />
+          <footer className="text-left text-muted">
+            <small>
+              *So far it has just explored Mars. No future plans to explore
+              other planets.
+            </small>
+          </footer>
         </Alert>
-      )
+      );
     }
   }
 
-  render(){
-
-    return(
-      <Fragment>
-        { this.renderAlert() }
-      </Fragment>
-    )
+  render() {
+    return <Fragment>{this.renderAlert()}</Fragment>;
   }
 }
 
-export default AlertMessage
+export default AlertMessage;
